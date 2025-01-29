a04c5f9bc6237d3695320884270aa58d16707746
Drian27 <muhamaddrian05@gmail.com>
2025-01-30 02:52:58 +0700

    push

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,7 +5,7 @@ import Breadcrumb from "@/components/Common/Breadcrumb";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Blog Page",
+  title: "Blog",
   description: "This is Blog Page",
   // other metadata
 };
